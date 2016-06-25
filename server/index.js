9957a4e7caf8db07c5fdc0ac3ca7b6fd5e1227d8
Devin <devinmui@yahoo.com>
2016-06-25 13:39:34 -0700

    index

--- server/index.js
+++ server/index.js
@@ -65,24 +65,35 @@ app.get('/analyse_data', function(req,res){
 
 app.post('/add_data', function(req, res){
 	var food = req.body.food
-	request('nutritionix_search', function(err, res, body){
+	request('https://api.nutritionix.com/v1_1/search/' + food, function(err, res, body){
 		if (!error && response.statusCode == 200) {
 			var id = body.hits[0]._id
-			request('nutritionix_item', function(err, res, body){
+			request('https://api.nutritionix.com/v1_1/item?id=' + id, function(err, res, body){
 				if (!error && response.statusCode == 200) {
+					var sodium = body.nf_sodium
+					var total_fat = body.nf_total_fat
+					var trans_fat = body.nf_trans_fatty_acid
+					var sat_fat = body.nf_saturated_fat
+					var cholesterol = body.nf_cholesterol
+					var carbs = body.nf_total_carbohydrate
+					var protein = body.nf_protein
+					var vit_a = body.nf_vitamin_a_dv
+					var vit_c = body.nf_vitamin_c_dv
+					var calcium = body.nf_calcium_dv
+					var iron = body.nf_iron_dv
 					// get the variables
 					var data = new Data({
-						sodium: Number,
-						total_fat: Number,
-						trans_fat: Number,
-						sat_fat: Number,
-						cholesterol: Number,
-						carbs: Number,
-						protein: Number,
-						vit_a: Number,
-						vit_c: Number,
-						calcium: Number,
-						iron: Number,
+						sodium: sodium,
+						total_fat: total_fat,
+						trans_fat: trans_fat,
+						sat_fat: sat_fat,
+						cholesterol: cholesterol,
+						carbs: carbs,
+						protein: protein,
+						vit_a: vit_a,
+						vit_c: vit_c,
+						calcium: calcium,
+						iron: iron,
 					})
 
 					data.save()
