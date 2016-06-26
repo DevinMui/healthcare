b04ff681c8408b3c1cb54c9ac395937b8a5defa3
Devin <devinmui@yahoo.com>
2016-06-26 04:31:02 -0700

    scheizer

--- alexa/index.js
+++ alexa/index.js
@@ -3,7 +3,10 @@
  */
 var request = require('request')
 var APP_ID = "amzn1.echo-sdk-ams.app.cd5c5752-bd92-4128-83b3-0c0e98109cb5"; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-
+var norm_total_sodium = 10
+var norm_total_total_fat = 10
+var norm_total_sat_fat = 5
+var norm_total_calcium = 5
 /**
  * The AlexaSkill prototype and helper functions
  */
@@ -16,7 +19,7 @@ var AlexaSkill = require('./AlexaSkill');
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
 var Hello = function () {
-    AlexaSkill.call(this, APP_ID);
+	AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
@@ -24,35 +27,83 @@ Hello.prototype = Object.create(AlexaSkill.prototype);
 Hello.prototype.constructor = Hello;
 
 Hello.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("Hello onSessionStarted requestId: " + sessionStartedRequest.requestId
-        + ", sessionId: " + session.sessionId);
-    // any initialization logic goes here
+	console.log("Hello onSessionStarted requestId: " + sessionStartedRequest.requestId
+		+ ", sessionId: " + session.sessionId);
+	// any initialization logic goes here
 };
-
+/*
 Hello.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("Hello onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Welcome to the Alexa Skills Kit, you can say hello";
-    var repromptText = "You can say hello";
-    response.ask(speechOutput, repromptText);
-};
+	console.log("Hello onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+	var speechOutput = "Welcome to the Alexa Skills Kit, you can say hello";
+	var repromptText = "You can say hello";
+	response.ask(speechOutput, repromptText);
+};*/
 
 Hello.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("Hello onSessionEnded requestId: " + sessionEndedRequest.requestId
-        + ", sessionId: " + session.sessionId);
-    // any cleanup logic goes here
+	console.log("Hello onSessionEnded requestId: " + sessionEndedRequest.requestId
+		+ ", sessionId: " + session.sessionId);
+	// any cleanup logic goes here
 };
 
 Hello.prototype.intentHandlers = {
-    // register custom intent handlers
-    "GetHealth": function (intent, session, response) {
-        //response.intent.latitude, response.intent.longitude
-        response.tellWithCard("Ayy lmao")
-    }
+	// are you healthy? what are you too high on and what should you consume
+	"GetHealth": function (intent, session, response) {
+
+		var options = {
+			url: 'http://16e94129.ngrok.io/get_data'
+		};
+		request.get(options, function(err, res, body){
+			if(!err){
+				var statement = ""
+				var total_total_fat = body.total_total_fat
+				var total_sodium = body.total_sodium
+				var total_sat_fat = body.total_sat_fat
+				var total_calcium = body.total_calcium
+				if(total_total_fat < norm_total_total_fat + 2){
+					// too little fat!
+					statement += "You should consume more total fat. "
+				} else if(norm_total_total_fat - 2 < total_total_fat){
+					// too much fat!
+					statement += "You're consuming too much total fat. "
+				} else {
+					statement += "You consumed just enough total fat. "
+				}
+
+				if(total_sodium < norm_total_sodium + 2){
+					statement += "You should consume more salt. Go play league of legends. "
+				} else if(norm_total_sodium - 2 < total_sodium) {
+					statement += "You fucking salty savage! "
+				} else {
+					statement += "You consumed just enough sodium. "
+				}
+
+				if(norm_total_sat_fat - 2 < total_sat_fat){
+					statement += "Eat a bit more saturated fat. "
+				} else if(total_sat_fat < norm_total_sat_fat + 2){
+					statement += "Are you our lord and savior, Gaben? Because you're eating a bit too much sat fat. "
+				} else{
+					statement += "You consumed just enough saturated fat. "
+				}
+
+				if(norm_total_calcium - 2 < total_calcium){
+					statement += "Mr. Skeletal doesn't like you. Eat some more calcium. "
+				} else if(total_calcium < norm_total_calcium + 2) {
+					statement += "Three spooky five me. You consumed a ton of calcium and became the next level Mr. Skeletal! "
+				} else {
+					statement += "You consumed just enough calcium. "
+				}
+
+				response.tellWithCard(statement);
+			} else
+				response.tellWithCard("Oh no. We could not reach the server")
+		})
+		//response.tellWithCard("Ayy lmao")
+	}
 };
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the Hello skill.
-    var skill = new Hello();
-    skill.execute(event, context);
+	// Create an instance of the Hello skill.
+	var skill = new Hello();
+	skill.execute(event, context);
 };
