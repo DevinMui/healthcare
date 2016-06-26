3b61432def7e002941de0a5ad07dec927e2b4aec
Jesse Liang <jesseliang@ymail.com>
2016-06-25 19:00:00 -0700

    pictures

--- health-ios/health-ios/ViewController.swift
+++ health-ios/health-ios/ViewController.swift
@@ -1,18 +1,25 @@
 //
 //  ViewController.swift
-//  health-ios
+//  swift-camera-basic
 //
-//  Created by Jesse Liang on 6/25/16.
-//  Copyright © 2016 Jesse Liang. All rights reserved.
+//  Created by Daniel Spiess on 10/16/15.
+//  Copyright © 2015 Daniel Spiess. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var currentImage: UIImageView!
+
+    
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set this controller as the camera delegate
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,52 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func takePicture(sender: UIButton) {
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .Camera
+                imagePicker.cameraCaptureMode = .Photo
+                presentViewController(imagePicker, animated: true, completion: {})
+            } else {
+                postAlert("Rear camera doesn't exist", message: "Application cannot access the camera.")
+            }
+        } else {
+            postAlert("Camera inaccessable", message: "Application cannot access the camera.")
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage:UIImage = (info[UIImagePickerControllerOriginalImage]) as? UIImage {
+            let selectorToCall = Selector("imageWasSavedSuccessfully:didFinishSavingWithError:context:")
+            UIImageWriteToSavedPhotosAlbum(pickedImage, self, selectorToCall, nil)
+        }
+        imagePicker.dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user saves an image
+        })
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user selects cancel
+        })
+    }
+    
+    func imageWasSavedSuccessfully(image: UIImage, didFinishSavingWithError error: NSError!, context: UnsafeMutablePointer<()>){
+        if let theError = error {
+            print("An error happened while saving the image = \(theError)")
+        } else {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.currentImage.image = image
+            })
+        }
+    }
+    
+    func postAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message,
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
